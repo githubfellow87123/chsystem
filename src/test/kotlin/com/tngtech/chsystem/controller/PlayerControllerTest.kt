a176416d83d2dd427b6970901b99f80130c95272
Benjamin Weimer <bw91@gmx.de>
2019-10-18 18:53:23 +0200

    beforeall/afterall

--- src/test/kotlin/com/tngtech/chsystem/controller/PlayerControllerTest.kt
+++ src/test/kotlin/com/tngtech/chsystem/controller/PlayerControllerTest.kt
@@ -1,6 +1,8 @@
 package com.tngtech.chsystem.controller
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -11,10 +13,25 @@ import org.springframework.http.HttpStatus
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PlayerControllerTest(@Autowired val restTemplate: TestRestTemplate) {
 
+    @BeforeAll
+    fun setup() {
+        println(">> Setup")
+    }
+
     @Test
     fun `Assert blog page title, content and status code`() {
         val entity = restTemplate.getForEntity<String>("/")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
         assertThat(entity.body).contains("<h1>Bernd</h1>")
+    }
+
+    @Test
+    fun `Assert article page title, content and status code`() {
+        println(">> TODO")
+    }
+
+    @AfterAll
+    fun teardown() {
+        println(">> Tear down")
     }
 }
