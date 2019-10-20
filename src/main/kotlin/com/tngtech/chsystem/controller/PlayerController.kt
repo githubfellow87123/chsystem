a9ae83ec7d41a4b50df76aaa015d3e5ee608d986
Benjamin Weimer <bw91@gmx.de>
2019-10-20 13:36:57 +0200

    get single player by id

--- src/main/kotlin/com/tngtech/chsystem/controller/PlayerController.kt
+++ src/main/kotlin/com/tngtech/chsystem/controller/PlayerController.kt
@@ -3,30 +3,51 @@ package com.tngtech.chsystem.controller
 import com.tngtech.chsystem.dao.PlayerRepository
 import com.tngtech.chsystem.entities.PlayerEntity
 import com.tngtech.chsystem.model.PlayerModel
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("players")
 class PlayerController(private val playerRepository: PlayerRepository) {
 
     @GetMapping
-    fun findAll(): List<PlayerModel> {
+    fun findAllPlayers(): List<PlayerModel> {
+
         val players = playerRepository.findAll()
 
-        return players.map { it.toPersonModel() }
+        return players.map { it.toPlayerModel() }
     }
 
+    @GetMapping
+    @RequestMapping("{id}")
+    fun findPlayerById(@PathVariable id: UUID): PlayerModel {
+
+        val player = playerRepository.findById(id)
+
+        if (player.isEmpty) {
+            throw PlayerNotFoundException(id)
+        }
+
+        return player.get().toPlayerModel()
+    }
+
+
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun insertPlayer(@RequestBody playerModel: PlayerModel): PlayerModel {
 
         val player = playerModel.toPlayerEntity()
 
         playerRepository.save(player)
 
-        return player.toPersonModel()
+        return player.toPlayerModel()
     }
 
-    fun PlayerEntity.toPersonModel() = PlayerModel(
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    class PlayerNotFoundException(id: UUID) : RuntimeException("No player found with id: $id")
+
+    fun PlayerEntity.toPlayerModel() = PlayerModel(
         id = id,
         name = name,
         createdAt = createdAt
